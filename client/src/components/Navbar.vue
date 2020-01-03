08f95ce2f7db68714c745df5995649aa10661600
Szymon Wojaczek <szymon@MacBook-Pro.local>
2020-01-03 17:36:28 +0100

    pages

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -2,11 +2,12 @@
   <v-card>
     <v-toolbar color="blue darken-4" dark>
       <v-app-bar-nav-icon></v-app-bar-nav-icon>
-      <v-toolbar-title>Title</v-toolbar-title>
+      <v-toolbar-title>ERP</v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn color="blue darken-3">Home</v-btn>
-      <v-btn color="blue darken-3">Add Employee</v-btn>
-      <v-btn color="blue darken-3">Add Contract</v-btn>
+      <v-btn color="blue darken-3" class="mx-2">Home</v-btn>
+      <v-btn color="blue darken-3" class="mx-2">Add Employee</v-btn>
+      <v-btn color="blue darken-3" class="mx-2">Add Contract</v-btn>
+      <v-btn color="blue darken-3" class="mx-2">Logout</v-btn>
     </v-toolbar>
   </v-card>
 </template>
---
+++ client/src/components/pages/NewContract.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h3>Hello from NewContract</h3>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'NewContract',
+  // components: {
+  //  NewContract
+  // }
+}
+</script>
+<style scoped>
+</style>
