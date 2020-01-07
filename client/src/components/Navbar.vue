0cb71239049f8920c7c771868ea4b62cc49a521b
Szymon Wojaczek <szymon@MacBook-Pro.local>
2020-01-07 17:07:11 +0100

    connect navbar with routing

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -4,9 +4,9 @@
       <v-app-bar-nav-icon></v-app-bar-nav-icon>
       <v-toolbar-title>ERP</v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn color="blue darken-3" class="mx-2">Home</v-btn>
-      <v-btn color="blue darken-3" class="mx-2">Add Employee</v-btn>
-      <v-btn color="blue darken-3" class="mx-2">Add Contract</v-btn>
+      <router-link to="/"><v-btn color="blue darken-3" class="mx-2">Home</v-btn></router-link>
+      <router-link to="NewEmployee"><v-btn color="blue darken-3" class="mx-2">Add Employee</v-btn></router-link>
+      <router-link to="NewContract"><v-btn color="blue darken-3" class="mx-2">Add Contract</v-btn></router-link>
       <v-btn color="blue darken-3" class="mx-2">Logout</v-btn>
     </v-toolbar>
   </v-card>
--- client/src/components/pages/NewEmployee.vue
+++ client/src/components/pages/NewEmployee.vue
@@ -1,14 +1,20 @@
 <template>
-  <v-app>
-    <div class="container">
+  <v-app class="grey">
+    <v-layout column>
+     <v-flex xs6>
+      <div class="container white elevation-3 form">
       <br>
-        <h3>Add New Employee</h3><br><br>
-        <input type="name" name="name" v-model="name" placeholder="Name"/><br>
-        <input type="surname" name="surname" v-model="surname" placeholder="Surname"/><br/>
-        <input type="birthdate" name="birthdate" v-model="birthdate" placeholder="Date of birth"/><br>
+        <h2>Add New Employee</h2><br><br>
+        <v-text-field type="text" name="name" v-model="name" placeholder="Name" outlined solo-inverted></v-text-field><br>
+        <v-text-field type="text" name="surname" v-model="surname" placeholder="Surname" outlined solo-inverted></v-text-field><br/>
+        <v-text-field type="date" name="birthdate" v-model="birthdate" placeholder="Date of birth" value="2018-07-22" outlined solo-inverted></v-text-field><br>
         <br>
-        <button @click="create">Register</button>
-    </div>
+        <div class="error" v-html="error"></div>
+        <br>
+        <v-btn class="cyan" @click="create">Add</v-btn>
+      </div>
+    </v-flex>
+   </v-layout>
   </v-app>
 </template>
 
@@ -20,22 +26,33 @@ export default {
     return{
       name:'',
       surname:'',
-      birthdate:''
+      birthdate:'',
+      error: ''
     }
   },
   methods:{
     async create() {
-     const response = await AuthenticationService.create({
-        name:this.name,        
-        surname:this.surname,        
-        birthdate:this.birthdate        
-      })
-      console.log(response.data)
+      try {
+        const response = await AuthenticationService.create({
+            name:this.name,        
+            surname:this.surname,        
+            birthdate:this.birthdate        
+          })
+       } catch (error){
+        this.error = error.response.data.error
+        console.log(error)
+       }
+       
     }
   }
 }
 </script>
 
 <style>
-
+.form{
+margin-top:2%;  
+text-align: center;
+padding:20px;
+background-color: aqua;
+}
 </style>
