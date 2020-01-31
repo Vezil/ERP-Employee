520def1a98d4528c263b2fa3b53e03f4a7d80d3a
Szymon Wojaczek <szymon@MacBook-Pro.local>
2020-01-31 18:08:20 +0100

    add test for 'get holidays for user' and 'add requests for holidays'

--- server/test/EmployeesController.js
+++ server/test/EmployeesController.js
@@ -5,9 +5,9 @@ const bcrypt = require('bcrypt');
 const saltRounds = 10;
 const expect = require('chai').expect;
 
-describe('users', () => {
-    describe('POST /employees', () => {
-        it('loggin when passing valid data', async () => {
+describe('user', () => {
+    describe('POST /login', () => {
+        it('login when passing valid data', async () => {
             const userData = {
                 email: 'user@erp.test',
                 password: 'password'
@@ -16,6 +16,31 @@ describe('users', () => {
             const response = await request.post(`/login`).send(userData);
 
             expect(response.body).to.have.property('token');
+
+            describe('GET /userHolidays', () => {
+                it('getting data of this user', async () => {
+                    await request
+                        .get(`/employees/${response.body.id}/holidays`)
+                        .set('Authorization', 'Bearer ' + response.body.token);
+
+                    expect(response.body);
+                });
+            });
+
+            describe('POST /userHolidays', () => {
+                it('sending request for holidays to admin', async () => {
+                    const userHolidaysRequest = {
+                        start_date: '2019-01-12',
+                        finish_date: '2019-01-13',
+                        user_id: response.body.id
+                    };
+                    await request
+                        .get(`/employees/${response.body.id}/holidays`)
+                        .send(userHolidaysRequest);
+
+                    expect(response.body);
+                });
+            });
         });
     });
 });
