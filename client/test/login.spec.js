36209eea3ec358ad228fb5ddca2bc01bf1889353
Szymon Wojaczek <szymon@MacBook-Pro.local>
2020-02-04 18:03:18 +0100

    first test on frontend

--- client/test/login.spec.js
+++ client/test/login.spec.js
@@ -1,11 +1,27 @@
-import { mount } from '@vue/test-utils';
+import { mount, createLocalVue } from '@vue/test-utils';
 import Login from '../src/components/pages/Login.vue';
+import expect from 'expect';
+import Vue from 'vue';
+import Vuetify from 'vuetify';
+
+Vue.use(Vuetify);
+
+const localVue = createLocalVue();
+
+let vuetify;
+let wrapper;
 
 describe('Login.vue', () => {
-    test('setup correctly', () => {
-        const wrapper = mount(Login);
+    vuetify = new Vuetify();
 
-        expect(wrapper);
-        console.log(wrapper);
+    beforeEach(() => {
+        wrapper = mount(Login, {
+            localVue,
+            vuetify
+        });
+    });
+
+    it('have two v-text-field ', () => {
+        expect(wrapper.findAll('.v-text-field').length).toBe(2);
     });
 });
